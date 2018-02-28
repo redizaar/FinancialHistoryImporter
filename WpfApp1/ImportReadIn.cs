5bea6d9eb29c8a82d8f97bd2114cc1651c3f6ea9
redizaar <pwnpwn90@gmail.com>
2018-02-28 17:35:21 +0100

    Add files via upload

---
+++ WpfApp1/ImportReadIn.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+namespace WpfApp1
+{
+    public class ImportReadIn
+    {
+        private List<string> path;
+        private string importType = "";
+        private string currentFileName;
+        private MainWindow mainWindow;
+        List<Transaction> transactions;
+
+        _Application excel = new _Excel.Application();
+        Workbook ReadWorkbook;
+        Worksheet ReadWorksheet;
+        public ImportReadIn(string _importType, List<string> _path,MainWindow _mainWindow,bool specifiedByUser)
+        {
+            path = _path;
+            importType = _importType;
+            mainWindow = _mainWindow;
+            if (path[0] != "FolderAdress")//a path wasn't choosen, useless if ( not in use )
+            {
+                for (int i = 0; i < path.Count; i++)
+                {
+                    string [] splittedFileName=path[i].Split('\\');
+                    int lastSplitIndex = splittedFileName.Length-1;
+                    currentFileName = splittedFileName[lastSplitIndex];
+                    ReadWorkbook = excel.Workbooks.Open(path[i]);
+                    ReadWorksheet = ReadWorkbook.Worksheets[1];
+                    if (importType=="Bank")
+                    {
+                        if (!specifiedByUser)
+                        {
+                            TemplateBankReadIn templateBank = new TemplateBankReadIn(this, ReadWorkbook, ReadWorksheet, mainWindow, false);
+                            //so far we got the Starting Row(of the transactions),Number of Columns, account number
+                            templateBank.readOutTransactionColumns(templateBank.getStartingRow(), templateBank.getNumberOfColumns());
+                        }
+                        else //userSpecified==true
+                        {
+                            TemplateBankReadIn templateBank = new TemplateBankReadIn(this, ReadWorkbook, ReadWorksheet, mainWindow, true);
+                            string startingRow = SpecifiedImport.getInstance(null,mainWindow).transactionsRowTextBox.Text.ToString();
+                            string dateColumn = SpecifiedImport.getInstance(null,mainWindow).dateColumnTextBox.Text.ToString();
+                            string commentColumn = SpecifiedImport.getInstance(null,mainWindow).commentColumnTextBox.Text.ToString();
+                            string accountNumberCB = SpecifiedImport.getInstance(null,mainWindow).accountNumberCB.SelectedItem.ToString();
+                            string transactionPriceCB = SpecifiedImport.getInstance(null,mainWindow).priceColumnCB.SelectedItem.ToString();
+                            string balanceCB = SpecifiedImport.getInstance(null,mainWindow).balanceColumnCB.SelectedItem.ToString();
+                            string balanceComboBocString = SpecifiedImport.getInstance(null, mainWindow).balanceColumnTextBox.Text.ToString();
+                            templateBank.readOutUserspecifiedTransactions(startingRow, dateColumn, commentColumn, accountNumberCB, transactionPriceCB, balanceCB, balanceComboBocString);
+                        }
+                    }
+                    else if(importType=="Stock")
+                    {
+                        TemplateStockReadIn templateStock = new TemplateStockReadIn(this,path);
+                    }
+                }
+                excel.Application.Quit();
+                excel.Quit();
+            }
+        }
+        ~ImportReadIn()
+        {
+            excel.Application.Quit();
+            excel.Quit();
+        }
+        public void addTransactions(List<Transaction> newTransactions)
+        {
+            this.transactions = newTransactions;
+            writeOutTransactions();
+        }
+        public void writeOutTransactions()
+        {
+            new ExportTransactions(transactions,mainWindow,currentFileName);
+        }
+    }
+}
---
+++ WpfApp1/TemplateStockReadIn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Data.SqlClient;
+using System.Data;
+namespace WpfApp1
+{
+    public class TemplateStockReadIn
+    {
+        private List<string> folderAddresses;
+        private ImportReadIn stockHandler;
+        public TemplateStockReadIn(ImportReadIn _stockHandler,List<string> _folderAddresses)
+        {
+            stockHandler = _stockHandler;
+            folderAddresses = _folderAddresses;
+        }
+    }
+}
