38bb54659fa3c72c3804b61f30243b710d533022
redizaar <pwnpwn90@gmail.com>
2018-03-04 17:16:05 +0100

    Add files via upload

--- TemplateStockReadIn.cs
+++ TemplateStockReadIn.cs
@@ -10,6 +10,7 @@ using System.Windows;
 using System.Data.SqlClient;
 using System.Data;
 using System.IO;
+using System.Net;
 
 namespace WpfApp1
 {
@@ -33,6 +34,132 @@ namespace WpfApp1
             stockWorksheet = workbook.Worksheets[1];
             int companyName = getCompanyColumn();
             int transactionDate = getDateColumn();
+            getHistoricalStockPrice(companyName,transactionDate);
+        }
+        //right know olny works for NASDAQ,NYSE
+        private void getHistoricalStockPrice(int companyColumn, int dateColumn)
+        {
+            List<string> companyNames = collectCompanyNames(companyColumn);
+            Dictionary<string, string> companyToDate = collectOldestShareDates(companyNames,companyColumn,dateColumn);
+            Dictionary<string, string> companyToTicker = new Dictionary<string, string>();
+            string companyNamesCSV;
+            using (var web = new WebClient())
+            {
+                var url = $"http://www.nasdaq.com/screening/companies-by-industry.aspx?render=download";
+                companyNamesCSV = web.DownloadString(url);
+            }
+            Regex reg = new Regex("\"([^\"]*?)\"");
+            var matches = reg.Matches(companyNamesCSV).
+                Cast<Match>()
+                .Select(m => m.Value)
+                .ToArray();
+            for (int i = 9; i < matches.Length; i += 9)
+            {
+                for(int j=0;j<companyNames.Count;j++)
+                {
+                    if(matches[i+1].Contains(companyNames[j]) || levenshteinDistance(companyNames[j],matches[i+1])==1)
+                    {
+                        string [] splitted = matches[i].Split('"');
+                        string ticker = "";
+                        for (int k = 0; k < splitted.Length; k++)
+                            ticker += splitted[k];
+                        companyToTicker.Add(companyNames[j], matches[i]);
+                        companyNames.Remove(companyNames[j]);
+                    }
+                }
+                //Console.WriteLine("Ticker: {0} -> Company name :{1} ", matches[i], matches[i + 1]);
+            }
+            /*
+             * minimum eltérés keresése
+            string amazon = "Amazon.com Inc.";
+            int temp;
+            int minimum=10;
+            int minIdx=10;
+            if (companyNames.Count>0)
+            {
+                for(int i=10;i<matches.Length;i+=9)
+                {
+                    temp = levenshteinDistance(amazon,matches[i]);
+                    if (temp<minimum)
+                    {
+                        minimum = temp;
+                        minIdx = i;
+                    }
+                }
+            }
+            */
+        }
+
+        private Dictionary<string, string> collectOldestShareDates(List<string> companyNames,int companyColumn, int dateColumn)
+        {
+            int blank_cell_counter = 0;
+            int row=1;
+            Dictionary<string, string> companyToOldestDate = new Dictionary<string, string>();
+            while(blank_cell_counter<2)
+            {
+                if(stockWorksheet.Cells[row,1].Value!=null)
+                {
+                    blank_cell_counter = 0;
+                }
+                else
+                {
+                    blank_cell_counter++;
+                }
+                row++;
+            }
+            int lastRow = row-2;
+            return companyToOldestDate;
+        }
+
+        public static int levenshteinDistance(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+            if (n == 0)
+            {
+                return m;
+            }
+            if (m == 0)
+            {
+                return n;
+            }
+            for (int i = 0; i <= n; d[i, 0] = i++)
+                ;
+            for (int j = 0; j <= m; d[0, j] = j++)
+                ;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+            return d[n, m];
+        }
+        private List<string> collectCompanyNames(int companyColumn)
+        {
+            List<string> returnValue = new List<string>();
+            int blank_cell_counter = 0;
+            int row = 1;
+            while(blank_cell_counter<2)
+            {
+                if(stockWorksheet.Cells[row,companyColumn].Value!=null)
+                {
+                    blank_cell_counter = 0;
+                    if(!returnValue.Contains(stockWorksheet.Cells[row, companyColumn].Value.ToString()))
+                        returnValue.Add(stockWorksheet.Cells[row, companyColumn].Value.ToString());
+                }
+                else
+                {
+                    blank_cell_counter++;
+                }
+                row++;
+            }
+            return returnValue;
         }
 
         public int getDateColumn()
@@ -40,17 +167,60 @@ namespace WpfApp1
             Regex dateRegex1 = new Regex(@"^20\d{2}.\d{2}.\d{2}");
             Regex dateRegex2 = new Regex(@"^20\d{2}-\d{2}-\d{2}");
             Regex dateRegex3 = new Regex(@"^20\d{2}.\s\d{2}.\s\d{2}");
-            Regex dateRegex4 = new Regex(@"^\d{2}-[a-zA-Z]{1}[\u0000-\u00FF]{1}[a-zA-Z]{2}.-\d{4}$");
-            if(dateRegex4.IsMatch(stockWorksheet.Cells[2,1].Value.ToString()))
+            Regex dateRegex4 = new Regex(@"^\d{2}-[\u0000-\u00FF]{3}.-\d{4}$"); // pl. 28-ápr-2018
+            Regex dateRegex5 = new Regex(@"^\d{2}-[\u0000-\u00FF]{4}.-\d{4}$"); // pl. 28-márc-2018
+            Regex dateRegex6 = new Regex(@"^\d{4}-[\u0000-\u00FF]{4}.-\d{2}$");
+            Regex dateRegex7 = new Regex(@"^\d{4}-[\u0000-\u00FF]{3}.-\d{2}$");
+            int blank_cell_counter = 0;
+            int row = 2;
+            int column = 1;
+            while (true)
             {
-                Console.WriteLine("Match");
-                return 0;
+                while (blank_cell_counter < 2)
+                {
+                    if (stockWorksheet.Cells[row, column].Value != null)
+                    {
+                        blank_cell_counter=0;
+                        if (dateRegex1.IsMatch(stockWorksheet.Cells[row, column].Value.ToString()) ||
+                            dateRegex2.IsMatch(stockWorksheet.Cells[row, column].Value.ToString()) ||
+                            dateRegex3.IsMatch(stockWorksheet.Cells[row, column].ToString()) ||
+                            dateRegex4.IsMatch(stockWorksheet.Cells[row, column].Value.ToString()) ||
+                            dateRegex5.IsMatch(stockWorksheet.Cells[row, column].Value.ToString()) ||
+                            dateRegex6.IsMatch(stockWorksheet.Cells[row, column].Value.ToString()) ||
+                            dateRegex7.IsMatch(stockWorksheet.Cells[row, column].Value.ToString()))
+                        {
+                            /*
+                             * It can be in the first column, but it got to have other values in that row
+                             * It can happen that it is the last value un the row
+                             * but in that case it isn't in the first column
+                             */
+                            if((stockWorksheet.Cells[row,column+1].Value!=null) || column!=1)
+                            {
+                                return column;
+                            }
+                        }
+                        else
+                        {
+                            column++;
+                        }
+                    }
+                    else
+                    {
+                        blank_cell_counter++;
+                    }
+                    row++;
+                }
+                column = 1;
+                if (stockWorksheet.Cells[row++, column].Value != null)
+                {
+                    blank_cell_counter = 0;
+                    row++;
+                }
+                else
+                {
+                    return 0;
+                }
             }
-            else
-            {
-                Console.WriteLine("Not matching");
-            }
-            return 0;
         }
 
         public int getCompanyColumn()
@@ -106,13 +276,11 @@ namespace WpfApp1
                 column = 1;
                 if(stockWorksheet.Cells[row++,column].Value!=null)
                 {
-                    //Console.WriteLine(row+++ ":" + column + " -> " + stockWorksheet.Cells[row++, column].Value.ToString());
                     blank_cell_counter = 0;
                     row++;
                 }
                 else
                 {
-                    Console.WriteLine(row + 1 + ":" + column + " -> null");
                     return 0;
                 }
             }
@@ -134,7 +302,6 @@ namespace WpfApp1
                 {
                     file.Add(sr.ReadLine());
                 }
-
                 return file.ToArray();
             }
         }
@@ -146,8 +313,8 @@ namespace WpfApp1
                 deleteTemporaryExcel();
             }
             */
-            workbook.Close();
-            excel.Quit();
+            //workbook.Close();
+            //excel.Quit();
         }
     }
 }
