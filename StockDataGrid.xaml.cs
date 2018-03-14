076e55f2da688ec57d57f243a20a8d0357b72b4f
redizaar <pwnpwn90@gmail.com>
2018-03-14 20:56:05 +0100

    Add files via upload

--- StockDataGrid.xaml.cs
+++ StockDataGrid.xaml.cs
@@ -127,15 +127,17 @@ namespace WpfApp1
                 }
                 dtb.AcceptChanges();
             }
-            addAttributesToTable(tableAttributes);
+            addAtributesToTable(tableAttributes);
         }
-        private void addAttributesToTable(List<Stock> tableAttributes)
+        private void addAtributesToTable(List<Stock> tableAttributes)
         {
-            storedStockDataGrid.Items.Clear();
-            foreach (var attribute in tableAttributes)
-                storedStockDataGrid.Items.Add(attribute);
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                storedStockDataGrid.Items.Clear();
+                foreach (var attribute in tableAttributes)
+                    storedStockDataGrid.Items.Add(attribute);
+            }));
         }
-
         private void switchToTable_Click(object sender, RoutedEventArgs e)
         {
             mainWindow.MainFrame.Content = new StockChart(mainWindow);
